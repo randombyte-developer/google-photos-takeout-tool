f3b8fc2b4c9d1bae7a78453fe6babea81bb02e6e
Sven Rahn <apps.randombyte@gmail.com>
2023-06-03 21:41:09 +0200

    Implement finding media and JSON file relations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,37 @@
 package de.randombyte.gptakeouttool
 
+import java.io.File
+
+const val ARGUMENT_COUNT = 1
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    if (args.size != ARGUMENT_COUNT) {
+        throw Exception("Got ${args.size} arguments but expected ${ARGUMENT_COUNT}!")
+    }
+
+    val sourceFolder = args[0]
+
+    val mediaFiles = hashSetOf<String>()
+    val jsonFiles = hashSetOf<String>()
+
+    File(sourceFolder).walkTopDown()
+        .filter { it.isFile }
+        .filterNot { it.extension.equals("mp", ignoreCase = true) }
+        .forEach { file ->
+            val pathWithoutExtension = "${file.parent}/${file.nameWithoutExtension}"
+            if (file.extension.equals("json", ignoreCase = true)) {
+                val withoutSecondExtension = pathWithoutExtension.substringBeforeLast(".")
+                jsonFiles += withoutSecondExtension
+            } else {
+                if (!pathWithoutExtension.contains("bearbeitet")) {
+                    mediaFiles += pathWithoutExtension
+                }
+            }
+        }
+
+    println("Found ${mediaFiles.size} media files and ${jsonFiles.size} JSON files")
+
+    val notFound = mediaFiles.filter { mediaFile -> !jsonFiles.contains(mediaFile) }
+
+    println("${notFound.size} files not found!")
 }
